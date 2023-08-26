da5a7fbab114494a40dd2452b1ddbcbdbb157b1e
Patryk Wychowaniec <pwychowaniec@pm.me>
2023-08-26 19:36:36 +0200

    Use cleaner implementation of no-op async waker

--- avr-tester/src/components/component_controller/waker.rs
+++ avr-tester/src/components/component_controller/waker.rs
@@ -1,16 +1,14 @@
-use std::ptr;
-use std::task::{RawWaker, RawWakerVTable, Waker};
-
-const NOOP_WAKER_VTABLE: RawWakerVTable = RawWakerVTable::new(waker_clone, noop, noop, noop);
+use std::sync::Arc;
+use std::task::{Wake, Waker};
 
 pub fn waker() -> Waker {
-    let raw = RawWaker::new(ptr::null(), &NOOP_WAKER_VTABLE);
-
-    unsafe { Waker::from_raw(raw) }
+    Arc::new(NoopWaker).into()
 }
 
-unsafe fn waker_clone(_: *const ()) -> RawWaker {
-    RawWaker::new(ptr::null(), &NOOP_WAKER_VTABLE)
-}
+struct NoopWaker;
 
-unsafe fn noop(_: *const ()) {}
+impl Wake for NoopWaker {
+    fn wake(self: Arc<Self>) {
+        //
+    }
+}
